217fb045f6bef9120a4e597fccecf5b03448a808
Andreas Wark <andreas.wark@gmx.de>
2023-12-23 14:49:14 +0100

    change colorscheme to dogrun

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,22 @@
 return {
-	{
-		"craftzdog/solarized-osaka.nvim",
-		branch = "osaka",
-		lazy = false,
-		priority = 1000,
-		opts = function()
-			return {
-				transparent = true,
-			}
-		end,
-		config = function()
-			vim.cmd.colorscheme "solarized-osaka"
-		end,
-	},
+  -- {
+  -- 	"craftzdog/solarized-osaka.nvim",
+  -- 	branch = "osaka",
+  -- 	lazy = false,
+  -- 	priority = 1000,
+  -- 	opts = function()
+  -- 		return {
+  -- 			transparent = true,
+  -- 		}
+  -- 	end,
+  -- 	config = function()
+  -- 		vim.cmd.colorscheme "solarized-osaka"
+  -- 	end,
+  -- },
+  {
+    "wadackel/vim-dogrun",
+    config = function()
+      vim.cmd.colorscheme "dogrun"
+    end,
+  }
 }
--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,6 +1,10 @@
 return {
   {
     "lukas-reineke/indent-blankline.nvim",
+    dependencies = {
+      -- "craftzdog/solarized-osaka"
+      "craftzdog/solarized-osaka.nvim",
+    },
     main = "ibl",
     -- event = "LazyFile",
     opts = {
@@ -32,7 +36,7 @@ return {
       local colors = require("solarized-osaka.colors").setup()
 
       hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = colors.violet900 } )
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = colors.violet900 })
       end)
 
       ibl.setup { indent = { highlight = highlight } }
