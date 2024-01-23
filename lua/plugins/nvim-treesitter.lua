104c30d02f0e7e8d62e64b4995fb5d10d5236867
Andreas Wark <andreas.wark@gmx.de>
2024-01-23 21:18:04 +0100

    add treesitter textobjects

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -11,6 +11,33 @@ return {
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = false },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<leader>si",
+            node_incremental = "<leader>si",
+            scope_incremental = "<leader>sc",
+            node_decremental = "<leader>sd",
+          },
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              -- ["ac"] = "@class.outer",
+              -- ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+              ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+            },
+            selection_modes = {
+              ["@parameter.outer"] = "v", -- charwise
+              ["@function.outer"] = "V", -- linewise
+              ["@class.outer"] = "<c-v>", -- blockwise
+            },
+          },
+        },
       })
     end,
   },
@@ -19,5 +46,11 @@ return {
     -- event = "LazyFile",
     enabled = true,
     opts = { mode = "cursor", max_lines = 3 },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    -- dependencies = {
+    --   "nvim/treesitter/nvim-treesitter",
+    -- },
   },
 }
