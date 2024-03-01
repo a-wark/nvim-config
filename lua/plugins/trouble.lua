6182037d08893db0bf9a0870a77d5786883a1aeb
Andreas Wark <andreas.wark@gmx.de>
2024-03-01 20:00:09 +0100

    configure trouble keybindings

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -10,21 +10,36 @@ return {
 
     keymap.set("n", "<leader>xx", function()
       trouble.toggle()
-    end)
-    keymap.set("n", "<leader>xW", function()
+    end, { desc = "Trouble toggle" })
+
+    keymap.set("n", "<leader>xw", function()
       trouble.toggle("workspace_diagnostics")
-    end)
+    end, { desc = "Trouble workspace diagnostics" })
+
     keymap.set("n", "<leader>xd", function()
       trouble.toggle("document_diagnostics")
-    end)
+    end, { desc = "Trouble document diagnostics" })
+
     keymap.set("n", "<leader>xq", function()
       trouble.toggle("quickfix")
-    end)
+    end, { desc = "Trouble quickfix" })
+
     keymap.set("n", "<leader>xl", function()
       trouble.toggle("loclist")
-    end)
+    end, { desc = "Trouble loclist" })
+
     keymap.set("n", "gR", function()
       trouble.toggle("lsp_references")
-    end)
+    end, { desc = "Trouble lsp references" })
+
+    keymap.set("n", "[x", function()
+      trouble.open()
+      trouble.previous({ skip_groups = true, jump = true })
+    end, { desc = "Trouble previous diagnostic" })
+
+    keymap.set("n", "]x", function()
+      trouble.open()
+      trouble.next({ skip_groups = true, jump = true })
+    end, { desc = "Trouble next diagnostic" })
   end,
 }
