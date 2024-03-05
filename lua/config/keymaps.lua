f43ca910c303922f62df972499f71778441073b4
Andreas Wark <andreas.wark@amperias.com>
2024-03-05 11:14:30 +0100

    adjust keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,11 +5,11 @@ keymap.set("i", "jk", "<ESC>")
 keymap.set("n", "x", '"_x"') -- delete single character without copying into register
 keymap.set("n", "<leader>p", '"_dP"') -- replace currenly selected text with default register without yanking it
 
-keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
-keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- vim maximizer
+keymap.set("n", "<leader>ei", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>e,", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>ee", "<C-w>=") -- make split windows equal width
+keymap.set("n", "<leader>en", ":close<CR>") -- close current split window
+keymap.set("n", "<leader>eu", ":MaximizerToggle<CR>") -- vim maximizer
 
 keymap.set("n", "<C-d>", "<C-d>zz") -- move half page down and center cursor
 keymap.set("n", "<C-u>", "<C-u>zz") -- move half page up and center cursor
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,31 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
+    event = "VimEnter",
     branch = "0.1.x",
     dependencies = {
-      "nvim-lua/plenary.nvim"
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        cond = function()
+          return vim.fn.executable("make") == 1
+        end,
+      },
+      {
+        "nvim-telescope/telescope-ui-select.nvim",
+      },
     },
     config = function()
       local telescope = require("telescope")
       local actions = require("telescope.actions")
 
       telescope.setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
         defaults = {
           mappings = {
             i = {
@@ -21,12 +37,51 @@ return {
         },
       })
 
+      -- load extensions
+      telescope.load_extension("fzf")
+      telescope.load_extension("ui-select")
+
       -- set keymaps
-      local keymap = vim.keymap
-      keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-      keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-      keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-      keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+      -- local keymap = vim.keymap
+      -- keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+      -- keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+      -- keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+      -- keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+
+      local builtin = require("telescope.builtin")
+      -- vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+      -- vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+      vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+      -- vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+      vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+      vim.keymap.set("n", "<leader>ss", builtin.live_grep, { desc = "[S]earch by Grep [S]tring" })
+      vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+      vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+      vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+
+      -- Slightly advanced example of overriding default behavior and theme
+      vim.keymap.set("n", "<leader>/", function()
+        -- You can pass additional configuration to telescope to change theme, layout, etc.
+        builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+          winblend = 20,
+          previewer = false,
+        }))
+      end, { desc = "[/] Fuzzily search in current buffer" })
+
+      -- -- Also possible to pass additional configuration options.
+      -- --  See `:help telescope.builtin.live_grep()` for information about particular keys
+      -- vim.keymap.set("n", "<leader>s/", function()
+      --   builtin.live_grep({
+      --     grep_open_files = true,
+      --     prompt_title = "Live Grep in Open Files",
+      --   })
+      -- end, { desc = "[S]earch [/] in Open Files" })
+
+      -- -- Shortcut for searching your neovim configuration files
+      -- vim.keymap.set("n", "<leader>sn", function()
+      --   builtin.find_files({ cwd = vim.fn.stdpath("config") })
+      -- end, { desc = "[S]earch [N]eovim files" })
     end,
-  }
+  },
 }
